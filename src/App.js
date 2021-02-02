81e551b374e707e4e74d34e73aa967aa2865efe0
villagef <filwyd123@gmail.com>
2021-02-02 19:13:55 +0100

    get API and save in localStorage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import './App.css';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 function App() {
+  const API = 'https://jsonplaceholder.typicode.com/posts';
+  const data = JSON.parse(localStorage.getItem("data"));
+
+  useEffect(() => {
+    (async function getData() {
+      const response = await axios.get(API);
+      localStorage.setItem('data', JSON.stringify(response.data))
+    })();
+  }, [data])
+
+
+
+  
+
+  // useEffect(() => {
+  //   localStorage.setItem('data', JSON.stringify(data))
+  // }, [data]);
+
+  // console.log(data);
+  
+
+
+  
+
   return (
     <div className="App">
 
